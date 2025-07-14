2b052fc1e6b47b7e224a959fbc35cedb75be9651
FABIAN GOMEZ <fabian.gomez.2310@gmail.com>
2025-07-14 00:01:58 -0500

    se configura cors para aceptar desarrollo local y produccion

--- src/app.js
+++ src/app.js
@@ -1,27 +1,30 @@
 const express = require('express');
-
 const cors = require('cors');
-
 require('dotenv').config();
 
-const movimientoRoutes = require('./routes/movimientos')
-
+const movimientoRoutes = require('./routes/movimientos');
 
 const app = express();
 
-app.use(cors());
+
+const corsOptions = {
+  origin: [
+    'http://localhost:5173', 
+    'https://control-finanzas-app.onrender.com' 
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 
 app.use('/api/movimientos', movimientoRoutes);
 
-//Ruta base
 
 app.get('/', (req, res) => {
-    res.send('API de control de Finanzas');
-})
-
+  res.send('API de control de Finanzas');
+});
 
 module.exports = app;
-
-
